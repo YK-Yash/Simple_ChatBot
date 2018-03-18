bc0181d2a85e9f8f6ffab03be3ee8e18c2162477
YK-Yash <yashkumar1998@gmail.com>
2018-03-18 21:59:54 +0530

    Final commit

--- chatbot2.py
+++ chatbot2.py
@@ -8,15 +8,8 @@ Created on Sun Mar 18 09:55:04 2018
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
 from chatterbot.trainers import ListTrainer
-import json
 
-import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
-
-a = '[{"16BIS0068":{"conversations":["Good morning, how are you?","I am doing well, how about you?","I am also good.","That is good to hear.","Yes it is."]}}]'
-jsonData = json.loads(a)
-jsonData[0]["16BIS0068"]
 
 # Importing the dataset
 dataset = pd.read_csv('student_dataset.csv', sep=',')
@@ -28,18 +21,26 @@ d_name = dataset['Name']
 d_marks = dataset['Marks']
 d_mobno = dataset['MobNo']
 
-
-# Uncomment the following line to enable verbose logging
-# import logging
-# logging.basicConfig(level=logging.INFO)
-
 # Create a new instance of a ChatBot
 bot = ChatBot(
     "Terminal",
     storage_adapter="chatterbot.storage.SQLStorageAdapter",
+    logic_adapters=[
+            "chatterbot.logic.MathematicalEvaluation",
+        {
+            'import_path': 'chatterbot.logic.BestMatch'
+            
+        },
+        {
+            'import_path': 'chatterbot.logic.LowConfidenceAdapter',
+            'threshold': 0.50,
+            'default_response': 'I am sorry, but I do not understand.'
+        }
+    ],
     input_adapter="chatterbot.input.TerminalAdapter",
     output_adapter="chatterbot.output.TerminalAdapter"
 )
+
 lowest_name = ''
 lowest_regno = ''
 topper_name = ''
@@ -48,7 +49,7 @@ no_of_failures = 0
 failures = ''
 no_of_people_90 = 0
 
-for i in range(0,2):
+for i in range(0,len(d_marks)):
     if d_marks[i] == min(d_marks):
         lowest_name = d_name[i]
         lowest_regno = d_regno[i]
@@ -59,18 +60,21 @@ for i in range(0,2):
     
     if d_marks[i] < 40:
         no_of_failures = no_of_failures + 1
-        failures = failures + ', ' + d_name +' ('+d_regno[i]+')'
+        failures = failures + ', '+ d_name[i] +' ('+d_regno[i]+')'
         
     if d_marks[i] >= 90:
         no_of_people_90 = no_of_people_90 + 1
 
+
+bot.set_trainer(ChatterBotCorpusTrainer)
+bot.train("chatterbot.corpus.english")
 bot.set_trainer(ListTrainer)
-for i in range(0,2):
+for i in range(0,len(d_marks)):
     
     bot.train([
-            "Give me the complete details of {}".format(d_regno),
+            "Give me the complete details of {}".format(d_regno[i]),
             "\nHere are the details:Registeration No.: {}; Name: {}  Age: {}  Sex: {}  Marks: {}  Mobile No.: {}".format(d_regno[i], d_name[i], d_age[i], d_sex[i], d_marks[i], d_mobno[i]),
-            "complete details of {}".format(d_regno),
+            "complete details of {}".format(d_regno[i]),
             "\nHere are the details:Registeration No.: {}; Name: {}  Age: {}  Sex: {}  Marks: {}  Mobile No.: {}".format(d_regno[i], d_name[i], d_age[i], d_sex[i], d_marks[i], d_mobno[i]),
     ])
     
@@ -135,7 +139,7 @@ bot.train([
    "{}".format(no_of_people_90)
         ])
     
-#bot.train("chatterbot.corpus.english")
+bot.train("chatterbot.corpus.english")
 
 CONVERSATION_ID = bot.storage.create_conversation()
 
@@ -154,23 +158,15 @@ def get_feedback():
         return get_feedback()
 
 
-print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nHi, I'm Silence - The Chatbot\n\n\n")
+print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nHi, I'm Silence - The Chatbot\nHere to help!\n\n")
 from chatterbot.utils import input_function
-# The following loop will execute each time the user enters input
+
 while True:
     try:
         input_statement = bot.input.process_input_statement()
         statement, response = bot.generate_response(input_statement, CONVERSATION_ID)
         bot.output.process_response(response)
         print('\n')
-        #print('"{}"\n'.format(response))
-#        if get_feedback():
-#            print("please input the correct one")
-#            response1 = bot.input.process_input_statement()
-#            bot.learn_response(response1, input_statement)
-#            bot.storage.add_to_conversation(CONVERSATION_ID, statement, response1)
-#            print("Responses added to bot!")
-
-    # Press ctrl-c or ctrl-d on the keyboard to exit
+       
     except (KeyboardInterrupt, EOFError, SystemExit):
         break
